828ac0c288dd3f8f218421e741da7e71f6853e28
Spencer (SC) <35679480+SpencerComputers@users.noreply.github.com>
2020-09-20 21:33:17 +1000

    Main Commit

---
+++ src/com/smacca/MultiplyDrops.java
@@ -0,0 +1,14 @@
+package com.smacca;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MultiplyDrops extends JavaPlugin{
+	@Override
+	public void onEnable() {
+		CommandMultiply commandMultiply = new CommandMultiply();
+		
+		this.getCommand("drops").setExecutor(commandMultiply);
+		getServer().getPluginManager().registerEvents(commandMultiply, this);
+	}
+
+}
